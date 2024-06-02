1512bc58016c839ab8b59273ac3de0c2df843feb
Beomgu <jb98967@gmail.com>
2024-06-01 22:15:07 -0400

    Workshop 4 Initialization

---
+++ Workshop/WS04/Canister.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Beomgu Jeon on 2024-06-01.
+// Name: Beomgu Jeon
+// Email: bgjeon@myseneca.ca
+// Student ID: 126428234
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <istream>
+#include <iomanip>
+#include <cstring>
+#include "Canister.h"
+using namespace std;
+namespace seneca {
+
+   
+}
---
+++ Workshop/WS04/Canister.h
@@ -0,0 +1,37 @@
+//
+// Created by Beomgu Jeon on 2024-06-01.
+// Name: Beomgu Jeon
+// Email: bgjeon@myseneca.ca
+// Student ID: 126428234
+
+#ifndef SENECA_CANISTER_H
+#define SENECA_CANISTER_H
+#include <iostream>
+namespace seneca {
+   class Canister {
+      char* m_contentName;
+      double m_diameter; // in centimeters 
+      double m_height;   // in centimeters
+      double m_contentVolume;  // in CCs
+      bool m_usable;
+      void setToDefault();
+      void setName(const char* Cstr);
+      bool isEmpty()const;
+      bool hasSameContent(const Canister& C)const;
+   public:
+      Canister();
+      Canister(const char* contentName);
+      Canister(double height, double diameter,
+         const char* contentName = nullptr);
+      ~Canister();
+      Canister& setContent(const char* contentName);
+      Canister& pour(double quantity);
+      Canister& pour(Canister&);
+      double volume()const;
+      std::ostream& display()const;
+      double capacity()const;
+      void clear();
+   };
+}
+
+#endif // !SENECA_BOX_H
