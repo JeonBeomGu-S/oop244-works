da72dd644f1c523bed641072ed90d838d529d153
Beomgu <jb98967@gmail.com>
2024-07-03 15:32:26 -0400

    MS1: Implement Menu class complete

---
+++ Project/MS1/Menu.h
@@ -0,0 +1,53 @@
+#ifndef SENECA_MENU_H__
+#define SENECA_MENU_H__
+#include <iostream>
+
+namespace seneca {
+    const unsigned int MAX_MENU_ITEMS = 20;
+
+    class MenuItem {
+        char *name { nullptr };
+
+        MenuItem();
+        MenuItem(const char *str);
+        ~MenuItem();
+
+        MenuItem(const MenuItem&) = delete;
+        MenuItem& operator=(const MenuItem&) = delete;
+
+        operator bool() const;
+        operator const char *() const;
+
+        void display() const;
+
+        friend class Menu;
+    };
+
+    class Menu {
+        char *title;
+        MenuItem* menuItems[MAX_MENU_ITEMS] = {nullptr, };
+        unsigned int noOfItems;
+
+        Menu(const Menu&) = delete;
+        Menu& operator=(const Menu&) = delete;
+
+    public:
+        Menu();
+        Menu(const char* str);
+        ~Menu();
+
+        void display() const;
+        unsigned int run();
+
+        unsigned int operator~();
+        Menu& operator<<(const char* menuItemContent);
+        friend std::ostream& operator<<(std::ostream& os, const Menu& menu);
+
+        operator int() const;
+        operator unsigned int() const;
+        operator bool() const;
+        const char* operator[](unsigned index) const;
+    };
+}
+
+#endif
