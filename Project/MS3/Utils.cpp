29d5b35972c72ef4788621ea0dd7cb16de602942
Beomgu <jb98967@gmail.com>
2024-07-04 20:32:53 -0400

    MS3: Initialize Date and Utils modules from MS1

---
+++ Project/MS3/Utils.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Beomgu Jeon on 2024-07-04.
+// Name: Beomgu Jeon
+// Email: bgjeon@myseneca.ca
+// Student ID: 126428234
+
+//
+
+#include "Utils.h"
+
+using namespace std;
+
+namespace seneca {
+    int getIntMM(int min, int max) {
+        int input = -1;
+        do {
+            cin >> input;
+            if (cin.fail() || input < min || input > max) {
+                cout << "Invalid Selection, try again: ";
+                input = -1;
+            }
+
+            cin.clear();
+            // clear input buffer
+            char ch = 'x';
+            while (ch != '\n') {
+                ch = cin.get();
+            }
+        } while (cin.fail() || input < min || input > max);
+
+        return input;
+    }
+}
