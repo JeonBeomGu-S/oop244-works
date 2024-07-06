1fd9dfa5ab9e20bddeda571a57f312ada2057b3b
Beomgu <jb98967@gmail.com>
2024-07-05 23:40:10 -0400

    MS3: Add Lib.h file
    - Lib.h holds all the general values of the system

---
+++ Project/MS3/Lib.h
@@ -0,0 +1,23 @@
+//
+// Created by Beomgu Jeon on 2024-07-04.
+// Name: Beomgu Jeon
+// Email: bgjeon@myseneca.ca
+// Student ID: 126428234
+
+//
+
+#ifndef SENECA_LIB_H
+#define SENECA_LIB_H
+
+const int SENECA_MAX_LOAN_DAYS = 15;
+// maximum number of day a publication can be borrowed with no penalty
+const int SENECA_TITLE_WIDTH = 30;
+// The width in which the title of a publication should be printed on the console
+const int SENECA_AUTHOR_WIDTH = 15;
+// The width in which the author name of a book should be printed on the console
+const int SENECA_SHELF_ID_LEN = 4;
+// The width in which the shelf id of a publication should be printed on the console
+const int SENECA_LIBRARY_CAPACITY = 5000;
+// Maximum number of publications the library can hold.
+
+#endif //SENECA_LIB_H
