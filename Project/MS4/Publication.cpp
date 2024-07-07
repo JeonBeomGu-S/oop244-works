fdc56501283189a39e855504bda1c53131d94ec3
Beomgu <jb98967@gmail.com>
2024-07-06 22:29:15 -0400

    MS4: Apply MS3's modifications
    - Exception handling in the copy constructor: 67ed97bdc6587727cebcf100c4c89ae4373f92d5
    - MS3: Update the read function logic: 1f596b262774b58492d3d269a03763ba21329e7e

--- Project/MS4/Publication.cpp
+++ Project/MS4/Publication.cpp
@@ -25,8 +25,10 @@ namespace seneca {
             m_title = nullptr;
         }
 
-        m_title = new char[strlen(p.m_title) + 1];
-        strcpy(m_title, p.m_title);
+        if (p.m_title != nullptr) {
+            m_title = new char[strlen(p.m_title) + 1];
+            strcpy(m_title, p.m_title);
+        }
         strcpy(m_shelfId, p.m_shelfId);
         m_membership = p.m_membership;
         m_libRef = p.m_libRef;
@@ -132,40 +134,49 @@ namespace seneca {
         m_date = Date();
 
         char buf[256];
+        char shelfId[SENECA_SHELF_ID_LEN + 1];
+        int membership = 0;
+        int libRef = -1;
+        Date date;
 
         if (conIO(is)) {
             cout << "Shelf No: ";
-            is >> m_shelfId;
+            is >> shelfId;
 
-            if (strlen(m_shelfId) != SENECA_SHELF_ID_LEN)
+            if (strlen(shelfId) != SENECA_SHELF_ID_LEN)
                 is.setstate(std::ios::failbit);
             else
                 is.ignore();
             cout << "Title: ";
             is.getline(buf, 256);
-            if (buf[0] != '\0') {
-                m_title = new char[strlen(buf) + 1];
-                strcpy(m_title, buf);
-            }
             cout << "Date: ";
-            is >> m_date;
-            if (!m_date)
+            is >> date;
+            if (!date)
                 is.setstate(std::ios::failbit);
         } else {
-            is >> m_libRef;
+            is >> libRef;
             is.ignore();
-            is.getline(m_shelfId, SENECA_SHELF_ID_LEN + 1, '\t');
+            is.getline(shelfId, SENECA_SHELF_ID_LEN + 1, '\t');
             is.getline(buf, 256, '\t');
+            is >> membership;
+            is.ignore();
+
+            is >> date;
+            if (!date)
+                is.setstate(std::ios::failbit);
+        }
+
+        if (!is.fail()) {
             if (buf[0] != '\0') {
                 m_title = new char[strlen(buf) + 1];
                 strcpy(m_title, buf);
             }
-            is >> m_membership;
-            is.ignore();
-
-            is >> m_date;
-            if (!m_date)
-                is.setstate(std::ios::failbit);
+            if (shelfId[0] != '\0') {
+                strcpy(m_shelfId, shelfId);
+            }
+            m_membership = membership;
+            m_libRef = libRef;
+            m_date = date;
         }
 
         return is;
